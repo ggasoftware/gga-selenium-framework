c65a0823e0214687cb356be2c7c2f8947b61f747
Roman Iovlev <roman_iovlev@epam.com>
2015-09-02 12:17:56 +0300

    2.0.0 Add getAllEnumValues

--- jdi-ui-test-framework/src/main/java/com/ggasoftware/jdi_ui_tests/utils/common/EnumUtils.java
+++ jdi-ui-test-framework/src/main/java/com/ggasoftware/jdi_ui_tests/utils/common/EnumUtils.java
@@ -3,6 +3,9 @@ package com.ggasoftware.jdi_ui_tests.utils.common;
 import com.ggasoftware.jdi_ui_tests.utils.usefulUtils.TryCatchUtil;
 
 import java.lang.reflect.Field;
+import java.util.List;
+
+import static edu.emory.mathcs.backport.java.util.Arrays.asList;
 
 /**
  * Created by Roman_Iovlev on 7/8/2015.
@@ -15,5 +18,8 @@ public class EnumUtils {
                 throw new Exception("Can't get Value from enum");
         } catch (Exception ex) { return enumWithValue.toString(); }
         return TryCatchUtil.tryGetResult(() -> (String) field.get(enumWithValue));
+    }
+    public static <T extends Enum> List<T> getAllEnumValues(T enumValue) {
+        return (List<T>) asList(enumValue.getDeclaringClass().getEnumConstants());
     }
 }
