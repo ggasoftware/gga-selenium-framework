fb9e244579037d71d403c6984b11bc2f81cea8d4
Roman Iovlev <roman_iovlev@epam.com>
2015-09-25 17:56:22 +0300

    add login publish save for Form

--- jdi-ui-test-framework/src/main/java/com/ggasoftware/jdi_ui_tests/implementation/selenium/elements/interfaces/complex/IForm.java
+++ jdi-ui-test-framework/src/main/java/com/ggasoftware/jdi_ui_tests/implementation/selenium/elements/interfaces/complex/IForm.java
@@ -12,10 +12,24 @@ public interface IForm<T> extends IComposite, ISetValue, IElement {
     /** Fills all elements on the form which implements SetValue interface and can be matched with fields in input entity */
     @JDIAction
     void fill(T entity);
+    /** Fill first setable field with valueand click on Button “submit” <br>
+     *  To use this option Form pageObject should have only one IButton Element */
+    @JDIAction
+    void submit(String text);
+    @JDIAction
+    default void add(String text) {submit(text);}
     /** Fill all SetValue elements and click on Button “submit” <br>
      *  To use this option Form pageObject should have only one IButton Element */
     @JDIAction
     void submit(T entity);
+    @JDIAction
+    default void login(T entity) { submit(entity); }
+    @JDIAction
+    default void add(T entity) { submit(entity); }
+    @JDIAction
+    default void publish(T entity) { submit(entity); }
+    @JDIAction
+    default void save(T entity) { submit(entity); }
     /** Fill all SetValue elements and click on Button specified button e.g. "Publish" or "Save" <br>
      *  To use this option Form pageObject should have button names in specific format <br>
      *  e.g. if you call "submit(user, "Publish") then you should have Element 'publishButton'. <br>
