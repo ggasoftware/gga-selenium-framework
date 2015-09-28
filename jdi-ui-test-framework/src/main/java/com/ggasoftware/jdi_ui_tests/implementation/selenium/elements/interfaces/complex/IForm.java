0621cf3d1eff89218d72fce26bc618892fce8df2
Roman Iovlev <roman_iovlev@epam.com>
2015-09-28 14:20:28 +0300

    Add more options for form

--- jdi-ui-test-framework/src/main/java/com/ggasoftware/jdi_ui_tests/implementation/selenium/elements/interfaces/complex/IForm.java
+++ jdi-ui-test-framework/src/main/java/com/ggasoftware/jdi_ui_tests/implementation/selenium/elements/interfaces/complex/IForm.java
@@ -23,13 +23,23 @@ public interface IForm<T> extends IComposite, ISetValue, IElement {
     @JDIAction
     void submit(T entity);
     @JDIAction
-    default void login(T entity) { submit(entity); }
+    default void login(T entity) { submit(entity, "login"); }
     @JDIAction
-    default void add(T entity) { submit(entity); }
+    default void add(T entity) { submit(entity, "add"); }
     @JDIAction
-    default void publish(T entity) { submit(entity); }
+    default void publish(T entity) { submit(entity, "publish"); }
     @JDIAction
-    default void save(T entity) { submit(entity); }
+    default void save(T entity) { submit(entity, "save"); }
+    @JDIAction
+    default void update(T entity) { submit(entity, "update"); }
+    @JDIAction
+    default void cancel(T entity) { submit(entity, "cancel"); }
+    @JDIAction
+    default void close(T entity) { submit(entity, "close"); }
+    @JDIAction
+    default void back(T entity) { submit(entity, "back"); }
+    @JDIAction
+    default void next(T entity) { submit(entity, "next"); }
     /** Fill all SetValue elements and click on Button specified button e.g. "Publish" or "Save" <br>
      *  To use this option Form pageObject should have button names in specific format <br>
      *  e.g. if you call "submit(user, "Publish") then you should have Element 'publishButton'. <br>
