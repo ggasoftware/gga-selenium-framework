e88ccbff64d9d8ca10040f315422ac821c27003e
Roman Iovlev <roman_iovlev@epam.com>
2015-10-02 15:54:10 +0300

    Correct logger name

--- jdi-ui-test-framework/src/main/java/com/ggasoftware/jdi_ui_tests/implementation/log4j/logger/Log4JLogger.java
+++ jdi-ui-test-framework/src/main/java/com/ggasoftware/jdi_ui_tests/implementation/log4j/logger/Log4JLogger.java
@@ -4,10 +4,10 @@ import com.ggasoftware.jdi_ui_tests.core.logger.base.AbstractLogger;
 import com.ggasoftware.jdi_ui_tests.core.logger.enums.BusinessInfoTypes;
 import com.ggasoftware.jdi_ui_tests.core.logger.enums.LogInfoTypes;
 import com.ggasoftware.jdi_ui_tests.core.logger.enums.LogLevels;
-import com.ggasoftware.jdi_ui_tests.core.settings.JDIData;
 import com.ggasoftware.jdi_ui_tests.core.utils.common.Timer;
 import org.apache.log4j.Logger;
 
+import static com.ggasoftware.jdi_ui_tests.core.settings.JDIData.frameworkName;
 import static org.apache.log4j.LogManager.getLogger;
 
 /**
@@ -30,7 +30,7 @@ public class Log4JLogger extends AbstractLogger {
         log4j.info(String.format("[%s] %s: %s", infoType, Timer.nowTimeShort(), message));
     }
 
-    private Logger log4j = getLogger(JDIData.frameworkName + " logger");
+    private Logger log4j = getLogger("log4j " + frameworkName + " logger");
 
     public Log4JLogger() { super(); }
     public Log4JLogger(LogLevels logLevel) { super(logLevel); }
