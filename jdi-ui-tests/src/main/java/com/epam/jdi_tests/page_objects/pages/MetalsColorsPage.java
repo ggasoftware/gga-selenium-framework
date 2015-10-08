d4dee9357c3df2b95b0642ce4d5936e5caaa826d
Maksim_Palchevskii <Maksim_Palchevskii@epam.com>
2015-10-08 13:29:36 +0300

    ComboBox Test added, should fix expand and run it.

--- jdi-ui-tests/src/main/java/com/epam/jdi_tests/page_objects/pages/MetalsColorsPage.java
+++ jdi-ui-tests/src/main/java/com/epam/jdi_tests/page_objects/pages/MetalsColorsPage.java
@@ -1,13 +1,16 @@
 package com.epam.jdi_tests.page_objects.pages;
 
 import com.epam.jdi_tests.enums.Colors;
+import com.epam.jdi_tests.enums.Metals;
 import com.epam.jdi_tests.enums.Nature;
 import com.epam.jdi_tests.page_objects.sections.Summary;
+import com.ggasoftware.jdi_ui_tests.implementation.selenium.elements.complex.ComboBox;
 import com.ggasoftware.jdi_ui_tests.implementation.selenium.elements.complex.Dropdown;
 import com.ggasoftware.jdi_ui_tests.implementation.selenium.elements.composite.Page;
 import com.ggasoftware.jdi_ui_tests.implementation.selenium.elements.interfaces.common.ILabel;
 import com.ggasoftware.jdi_ui_tests.implementation.selenium.elements.interfaces.common.IText;
 import com.ggasoftware.jdi_ui_tests.implementation.selenium.elements.interfaces.complex.ICheckList;
+import com.ggasoftware.jdi_ui_tests.implementation.selenium.elements.interfaces.complex.IComboBox;
 import com.ggasoftware.jdi_ui_tests.implementation.selenium.elements.interfaces.complex.IDropDown;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
@@ -31,4 +34,5 @@ public class MetalsColorsPage extends Page {
     @FindBy(css = "#elements-checklist label")
     public ICheckList<Nature> nature;
 
+    public IComboBox<Metals> comboBox = new ComboBox<>(By.cssSelector(".metals .filter-option"), By.cssSelector(".metals li span"));
 }
