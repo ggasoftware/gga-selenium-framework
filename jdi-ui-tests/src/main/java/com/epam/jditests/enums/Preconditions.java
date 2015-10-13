c8549cc8dd1914453ba21663dd785cce6f7430e5
Roman Iovlev <roman_iovlev@epam.com>
2015-10-13 14:22:22 +0300

    Correct preconditions

--- jdi-ui-tests/src/main/java/com/epam/jditests/enums/Preconditions.java
+++ jdi-ui-tests/src/main/java/com/epam/jditests/enums/Preconditions.java
@@ -8,13 +8,10 @@ import static com.epam.jditests.CommonData.TEST_DATE;
 import static com.epam.jditests.entities.User.DEFAULT_USER;
 import static com.epam.jditests.page_objects.EpamJDISite.contactFormPage;
 import static com.epam.jditests.page_objects.EpamJDISite.dates;
-<<<<<<< HEAD
 import static com.ggasoftware.jdiuitests.core.settings.JDISettings.domain;
+import static com.ggasoftware.jdiuitests.core.settings.JDISettings.getDriver;
 import static com.ggasoftware.jdiuitests.implementation.selenium.elements.composite.Page.getUrl;
 import static com.ggasoftware.jdiuitests.implementation.selenium.elements.composite.Page.openUrl;
-=======
-import static com.ggasoftware.jdiuitests.core.settings.JDISettings.getDriver;
->>>>>>> refs/remotes/origin/master
 
 /**
  * Created by 12345 on 03.06.2015.
@@ -22,22 +19,22 @@ import static com.ggasoftware.jdiuitests.core.settings.JDISettings.getDriver;
 public enum Preconditions {
     HOME_PAGE("index.htm"),
     CONTACT_PAGE("page1.htm"),
-    CONTACT_PAGE_WITH_FILLED_FIELDS(() -> checkUrl("page1.htm"), () -> {
+    CONTACT_PAGE_FILLED(() -> checkUrl("page1.htm"), () -> {
         openUri("page1.htm");
         contactFormPage.name.newInput(DEFAULT_USER.name);
         contactFormPage.lastName.newInput(DEFAULT_USER.lastName);
         contactFormPage.description.newInput(DEFAULT_USER.description);
     }),
     METALS_AND_COLORS_PAGE("page2.htm"),
-//    DATES_PAGE("page4.htm");
+    DATES_PAGE("page4.htm"),
     SUPPORT_PAGE("page3.htm"),
-    DATES_PAGE(() -> checkUrl("page4.html"), () -> {
-    	openUri("page4.htm");
+    DATES_PAGE_FILLED(() -> checkUrl("page4.html"), () -> {
+        openUri("page4.htm");
         WebElement datePicker = getDriver().findElement(dates.datepicker.getLocator());
         datePicker.clear();
         datePicker.sendKeys(TEST_DATE);
     });
-	public String _htmlPageName;
+    public String _htmlPageName;
     public JFuncT<Boolean> checkAction;
     public JAction moveToAction;
 
@@ -46,7 +43,7 @@ public enum Preconditions {
         this.moveToAction = moveToAction;
     }
     Preconditions(final String uri) {
-        this(() -> checkUrl(uri), () -> openUri(uri));
+        this(() -> false/*checkUrl(uri)*/, () -> openUri(uri));
         _htmlPageName = uri;
     }
     private static boolean checkUrl(final String uri) {
