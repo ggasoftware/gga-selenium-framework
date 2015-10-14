0a06aa1268be56c44590766bebc6e896d27d963c
Roman Iovlev <roman_iovlev@epam.com>
2015-10-14 13:43:23 +0300

    exclude tests

--- HEM_Tests/src/main/java/com/epam/hem/tests/pageObjects/sections/base/ListView.java
+++
@@ -1,9 +0,0 @@
-package com.epam.hem.tests.pageObjects.sections.base;
-
-import com.ggasoftware.jdiuitests.implementation.selenium.elements.composite.Section;
-
-/**
- * Created by Pavel_Shcherbakov1 on 2015-10-12
- */
-public class ListView extends Section {
-}
--- HEM_Tests/src/main/java/com/epam/hem/tests/pageObjects/sections/projects/CloneProjectForm.java
+++
@@ -1,8 +0,0 @@
-package com.epam.hem.tests.pageObjects.sections.projects;
-
-/**
- * Created by Pavel_Shcherbakov1 on 2015-10-12
- */
-public class CloneProjectForm extends ProjectForm {
-
-}
